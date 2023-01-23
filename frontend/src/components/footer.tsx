c0c54aa63a1535a97448b3778a713f4a4fabaa2e
mkmcary <mkmcary@gmail.com>
2023-01-23 10:56:44 -0500

    Add TMDB disclosure.

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -20,6 +20,11 @@ function Footer() {
           Best with Butter is a Web App Developed by Matthew Martin for Use in
           Tracking Movies and TV Shows to Watch.
         </Typography>
+
+        <Typography variant="caption">
+          This product uses the TMDB API but is not endorsed or certified by
+          TMDB.
+        </Typography>
         <Box>
           <BWBLogo />
         </Box>
