d261278d3a3cfd3061e55668af09c496974067e9
mkmcary <mkmcary@gmail.com>
2023-02-01 16:52:06 -0500

    Stylized movie listings.

--- frontend/src/components/ListTable/index.tsx
+++ frontend/src/components/ListTable/index.tsx
@@ -10,53 +10,27 @@ import {
   Paper,
   Typography,
 } from "@mui/material";
-import { MovieDetails } from "../../routes/search";
+import MovieNode, { MovieDetails } from "../MovieNode";
 
 interface ListTableProps {
   movies: MovieDetails[];
 }
 
-const headers = ["", "Title", "Release Date", "Overview", "Genres", "TMDB ID"];
-
 export default function ListTable({ movies }: ListTableProps) {
   return (
-    <TableContainer component={Paper} sx={{ borderRadius: 5 }}>
+    <TableContainer
+      component={Paper}
+      sx={{ borderRadius: 5, backgroundColor: "#fef9e1" }}
+    >
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow sx={{ backgroundColor: "#F21B42" }}>
-            {headers.map((header) => (
-              <TableCell>
-                <Typography
-                  variant="h6"
-                  sx={{ fontWeight: 700, color: "white" }}
-                >
-                  {header}
-                </Typography>
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
         <TableBody>
           {movies != null &&
             movies.map((movie) => {
-              const genres = movie.genres.join(", ");
               return (
-                <TableRow
-                  key={movie.title}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
+                <TableRow>
                   <TableCell>
-                    <img src={movie.poster_url} style={{ width: 100 }} />
+                    <MovieNode movie={movie} />
                   </TableCell>
-                  <TableCell>
-                    <Typography sx={{ fontWeight: 700 }}>
-                      {movie.title}
-                    </Typography>
-                  </TableCell>
-                  <TableCell>{movie.release_date}</TableCell>
-                  <TableCell>{movie.overview}</TableCell>
-                  <TableCell>{genres}</TableCell>
-                  <TableCell>{movie.id}</TableCell>
                 </TableRow>
               );
             })}
--- frontend/src/components/MovieNode/index.tsx
+++ frontend/src/components/MovieNode/index.tsx
@@ -1,3 +1,82 @@
-function MovieNode() {}
+import { Box, Grid, Typography, Divider } from "@mui/material";
+
+export interface MovieDetails {
+  id: number;
+  title: string;
+  overview: string;
+  genres: string[];
+  poster_url: string;
+  release_date: string;
+}
+
+interface MovieNodeProps {
+  movie: MovieDetails;
+}
+
+function MovieNode({ movie }: MovieNodeProps) {
+  const genres = movie.genres.join(", ");
+
+  const formatDateString = (date: string) => {
+    let months: { [name: string]: string } = {
+      m01: "January",
+      m02: "February",
+      m03: "March",
+      m04: "April",
+      m05: "May",
+      m06: "June",
+      m07: "July",
+      m08: "August",
+      m09: "September",
+      m10: "October",
+      m11: "November",
+      m12: "December",
+    };
+
+    let dateSplit = date.split("-");
+    const year = dateSplit[0];
+    const monthStr = `m${dateSplit[1]}`;
+    const month = months[monthStr];
+    const day = dateSplit[2];
+    return `${month} ${day}, ${year}`;
+  };
+
+  return (
+    <Box>
+      <Grid container>
+        <Grid item xs={4}>
+          <img
+            src={movie.poster_url}
+            style={{ width: 300, borderRadius: 10 }}
+          />
+        </Grid>
+        <Grid item xs={8}>
+          <Box sx={{ m: "10px" }}>
+            <Typography variant="h4" sx={{ fontWeight: 700 }}>
+              {movie.title}
+            </Typography>
+          </Box>
+
+          <Divider />
+
+          <Box sx={{ m: "10px", minHeight: "300px" }}>
+            <Typography variant="h6" sx={{ fontWeight: 600, mb: "5px" }}>
+              {formatDateString(movie.release_date)}
+            </Typography>
+            <Typography variant="h6" sx={{ fontWeight: 600, mb: "5px" }}>
+              {genres}
+            </Typography>
+            <Typography>{movie.overview}</Typography>
+          </Box>
+
+          <Divider />
+
+          <Box sx={{ m: "10px" }}>
+            <Typography variant="caption">TMDB ID: {movie.id}</Typography>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
 
 export default MovieNode;
