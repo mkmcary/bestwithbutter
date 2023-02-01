c59154f75c0f85ce0607acf71d9ab1288f251af0
Matthew Martin <mkmcary@gmail.com>
2023-02-01 13:35:40 -0500

    Implement search for movies. (#2)

--- frontend/src/components/listTable.tsx
+++ frontend/src/components/listTable.tsx
@@ -8,40 +8,58 @@ import {
   TableHead,
   TableRow,
   Paper,
+  Typography,
 } from "@mui/material";
-import { Movie } from "../routes/mylist";
+import { MovieDetails } from "../routes/search";
 
 interface ListTableProps {
-  movies: Movie[];
+  movies: MovieDetails[];
 }
+
+const headers = ["", "Title", "Release Date", "Overview", "Genres", "TMDB ID"];
 
 export default function ListTable({ movies }: ListTableProps) {
   return (
-    <TableContainer component={Paper}>
+    <TableContainer component={Paper} sx={{ borderRadius: 5 }}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
-          <TableRow>
-            <TableCell>Title</TableCell>
-            <TableCell>Year</TableCell>
-            <TableCell>Genres</TableCell>
+          <TableRow sx={{ backgroundColor: "#F21B42" }}>
+            {headers.map((header) => (
+              <TableCell>
+                <Typography
+                  variant="h6"
+                  sx={{ fontWeight: 700, color: "white" }}
+                >
+                  {header}
+                </Typography>
+              </TableCell>
+            ))}
           </TableRow>
         </TableHead>
         <TableBody>
-          {movies.map((movie) => {
-            const genres = movie.genres.join(", ");
-            return (
-              <TableRow
-                key={movie.title}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {movie.title}
-                </TableCell>
-                <TableCell>{movie.year}</TableCell>
-                <TableCell>{genres}</TableCell>
-              </TableRow>
-            );
-          })}
+          {movies != null &&
+            movies.map((movie) => {
+              const genres = movie.genres.join(", ");
+              return (
+                <TableRow
+                  key={movie.title}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell>
+                    <img src={movie.poster_url} style={{ width: 100 }} />
+                  </TableCell>
+                  <TableCell>
+                    <Typography sx={{ fontWeight: 700 }}>
+                      {movie.title}
+                    </Typography>
+                  </TableCell>
+                  <TableCell>{movie.release_date}</TableCell>
+                  <TableCell>{movie.overview}</TableCell>
+                  <TableCell>{genres}</TableCell>
+                  <TableCell>{movie.id}</TableCell>
+                </TableRow>
+              );
+            })}
         </TableBody>
       </Table>
     </TableContainer>
