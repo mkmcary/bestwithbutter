ca3f373ef3e0a9cab31f86acd1dbf3fb6ff397aa
mkmcary <mkmcary@gmail.com>
2023-03-22 17:21:15 -0400

    Added image.

--- frontend/src/routes/Home/index.tsx
+++ frontend/src/routes/Home/index.tsx
@@ -10,64 +10,109 @@ function HomePage() {
   let iconDelay = Math.floor(Math.random() * 5001) - 5000;
 
   return (
-    <Box sx={{ minHeight: "70vh", p: "20px" }}>
-      <Box
-        sx={{
-          width: "100%",
-          justifyContent: "center",
-          alignItems: "center",
-          textAlign: "center",
-          p: "10px",
-        }}
-      >
-        <p className="word">Check out my GitHub</p>
-
-        {/* Exploding Link */}
-        <Box
-          sx={{
-            display: "inline-block",
+    <Box
+      sx={{
+        my: "20px",
+        py: "20px",
+        borderTop: "1px solid #EEEEEE",
+        borderBottom: "1px solid #EEEEEE",
+        alignItems: "center",
+        justifyContent: "center",
+        display: "flex",
+      }}
+    >
+      <Box sx={{ display: "flex" }}>
+        {/* Image */}
+        <div
+          style={{
+            width: "30%",
+            maxWidth: "500px",
+            padding: "0px 20px 0px 20px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
           }}
         >
-          <div className="line">
-            <a
-              href="https://github.com/mkmcary"
-              target="_blank"
-              className="word fancy"
+          <img
+            src="/matthew.jpg"
+            style={
+              {
+                // width: "100%",
+              }
+            }
+          />
+        </div>
+
+        {/* Text */}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <Box
+            sx={{
+              width: "100%",
+              justifyContent: "center",
+              textAlign: "center",
+              p: "10px",
+              // m: "0px auto",
+            }}
+          >
+            <p className="word fancy">Check out my GitHub</p>
+
+            {/* Exploding Link */}
+            <Box
+              sx={{
+                display: "inline-block",
+              }}
             >
-              <span className="outer">
-                <span
-                  className="inner"
-                  style={{ animationDelay: `${iconDelay}ms` }}
+              <div className="line">
+                <a
+                  href="https://github.com/mkmcary"
+                  target="_blank"
+                  className="word fancy"
                 >
-                  <span className="letter" style={{ animationDelay: "0ms" }}>
-                    <FontAwesomeIcon icon={faCodeBranch} />
-                  </span>
-                </span>
-              </span>
-
-              {linkText.split("").map((letter, index) => {
-                let innerDelay = Math.floor(Math.random() * 5001) - 5000;
-                let letterDelay = 1000 * (index + 1);
-
-                return (
-                  <span className="outer" key={index}>
+                  <span className="outer">
                     <span
                       className="inner"
-                      style={{ animationDelay: `${innerDelay}ms` }}
+                      style={{ animationDelay: `${iconDelay}ms` }}
                     >
                       <span
                         className="letter"
-                        style={{ animationDelay: `${letterDelay}ms` }}
+                        style={{ animationDelay: "0ms" }}
                       >
-                        {letter}
+                        <FontAwesomeIcon icon={faCodeBranch} />
                       </span>
                     </span>
                   </span>
-                );
-              })}
-            </a>
-          </div>
-        </Box>
+
+                  {linkText.split("").map((letter, index) => {
+                    let innerDelay = Math.floor(Math.random() * 5001) - 5000;
+                    let letterDelay = 1000 * (index + 1);
+
+                    return (
+                      <span className="outer" key={index}>
+                        <span
+                          className="inner"
+                          style={{ animationDelay: `${innerDelay}ms` }}
+                        >
+                          <span
+                            className="letter"
+                            style={{ animationDelay: `${letterDelay}ms` }}
+                          >
+                            {letter}
+                          </span>
+                        </span>
+                      </span>
+                    );
+                  })}
+                </a>
+              </div>
+            </Box>
+          </Box>
+        </div>
       </Box>
     </Box>
   );
